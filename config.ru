3c4a1578b80767d35b5ab9f147990fb3bf581b98
EricG <elgodet@gmail.com>
2014-06-08 23:37:31 -0400

    Initial commit

---
+++ config.ru
@@ -0,0 +1,6 @@
+# This file is used by Rack-based servers to start the application.
+
+require ::File.expand_path('../config/environment',  __FILE__)
+run Rails.application
+
+set :database, ENV['db_development'] || 'postgres://localhost/db_development'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = 'ee7662e9dd5c8a8722d620effe925a13d760045a19d504b4dfedd75045ead66235adb69e554c92a58d8711fadbaee7f7637bd7db51000aca5aece99a8d5d5469'
